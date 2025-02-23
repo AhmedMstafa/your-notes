b9052040e8a5b8dd875006e341b2e15961b60ad6
AhmedMstafa <ahmed.mostafa.mail.1@gmail.com>
2025-02-23 09:45:00 +0200

    Edit modal

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -12,7 +12,11 @@ export default function Modal({ logoutHandler, navigateTo }) {
       className="absolute top-[62px] w-screen h-content-hight element-center bg-black/80 backdrop-invert backdrop-opacity-10 md:bg-transparent md:backdrop-blur-none md:backdrop-opacity-0"
     >
       <div className="container relative h-full w-full element-center ">
-        <div className=" text-center w-full flex flex-col gap-3 justify-center px-[20px] absolute md:px-[10px]  md:w-[263px] md:h-[231px] md:top-[17px] md:right-[10px] md:rounded-md md:bg-white md:shadow-md">
+        <div
+          className={` text-center w-full flex flex-col gap-3 justify-center px-[20px] absolute md:px-[10px]  md:w-[263px] md:h-[231px] md:top-[17px] ${
+            isEnglish ? 'md:right-[10px]' : 'md:left-[10px]'
+          } md:rounded-md md:bg-white md:shadow-md`}
+        >
           <p className="text-main-color text-[25px] mb-[10px]">
             {isEnglish ? 'hi' : 'مرحباً'} {userName}
           </p>
