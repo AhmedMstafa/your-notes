b96fec0353ae4906e65bbc1d1053692fd94b1b7e
AhmedMstafa <ahmed.mostafa.mail.1@gmail.com>
2025-02-25 17:32:37 +0200

    Edit notes page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,18 @@
 import { Outlet } from 'react-router-dom';
 import Header from '../components/Header';
+import { useDispatch } from 'react-redux';
+import { useEffect } from 'react';
+import { fetchUserData } from '../store/user-slice';
+import { useLocation } from 'react-router-dom';
 export default function Home() {
+  
+  const location = useLocation();
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchUserData());
+  }, [location]);
+
   return (
     <>
       <Header />
--- src/store/user-slice.js
+++ src/store/user-slice.js
@@ -1,4 +1,27 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import { getAuthToken } from '../util/auth';
+
+export const fetchUserData = createAsyncThunk(
+  'user/fetchUserData',
+  async () => {
+    const token = getAuthToken();
+
+    const userResponse = await fetch(
+      'https://your-notes.vercel.app/api/users',
+      {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    );
+
+    const userData = await userResponse.json();
+
+    return userData.data.users[0];
+  }
+);
 
 const userSlice = createSlice({
   name: 'user',
@@ -14,6 +37,11 @@ const userSlice = createSlice({
     updateUserInfo: (state, action) => {
       state.userInfo = { ...state.userInfo, ...action.payload.userInfo };
     },
+  },
+  extraReducers: (builder) => {
+    builder.addCase(fetchUserData.fulfilled, (state, action) => {
+      state.userInfo = { ...state.userInfo, ...action.payload };
+    });
   },
 });
 
